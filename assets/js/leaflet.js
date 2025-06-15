8418b843ffd93921aeee843d1029a4e33442f91c
Strappazzon <im@strappazzon.xyz>
2025-06-15 09:20:36 +0200

    Use Markdown in markers content

--- assets/js/leaflet.js
+++ assets/js/leaflet.js
@@ -6,6 +6,13 @@ document.addEventListener('DOMContentLoaded', () => {
     return [ mapBounds[ 1 ][ 0 ] - y, x ];
   }
 
+  // Convert Markdown to HTML
+  const md = window.markdownit({
+    breaks: true,
+    html: true,
+    quotes: '""\'\'',
+  });
+
   // Config
 
   let mapBounds = [
@@ -95,85 +102,80 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // Markers: Definitions
 
-  let mapCommsBridge1 = L.marker(convertCoords(2233, 1155), { icon: commsIcon }).bindPopup('<h1>Resources</h1><p>Bridge</p>');
-  let mapCommsBridge2 = L.marker(convertCoords(2271, 1184), { icon: commsIcon }).bindPopup('<h1>Fishy</h1><p>Bridge</p>');
-  let mapCommsBridge3 = L.marker(convertCoords(2240, 1217), { icon: commsIcon }).bindPopup('<h1>Sarah</h1><p>Bridge</p>');
-  let mapCommsBridge4 = L.marker(convertCoords(2359, 1210), { icon: commsIcon }).bindPopup('<h1>Reunited</h1><p>Bridge</p>');
-  let mapCommsBridge5 = L.marker(convertCoords(2335, 1286), { icon: commsIcon }).bindPopup('<h1>Maral</h1><p>Bridge</p>');
-  let mapCommsBridge6 = L.marker(convertCoords(2229, 1338), { icon: commsIcon }).bindPopup('<h1>Marauders</h1><p>Bridge</p>');
-  let mapCommsHostiles1 = L.marker(convertCoords(1630, 2257), { icon: commsIcon }).bindPopup('<h1>Negotiations</h1><p>Hostiles</p>');
-  let mapCommsHostiles2 = L.marker(convertCoords(456, 1283), { icon: commsIcon }).bindPopup('<h1>Tech</h1><p>Hostiles</p>');
-  let mapCommsHostiles3 = L.marker(convertCoords(2721, 1379), { icon: commsIcon }).bindPopup('<h1>Meret</h1><p>Hostiles</p>');
-  let mapCommsHostiles4 = L.marker(convertCoords(2869, 1185), { icon: commsIcon }).bindPopup('<h1>Collab</h1><p>Hostiles</p>');
-  let mapCommsHostiles5 = L.marker(convertCoords(3556, 1326), { icon: commsIcon }).bindPopup('<h1>Clinic</h1><p>Hostiles</p>');
-  let mapCommsHostiles6 = L.marker(convertCoords(3700, 1257), { icon: commsIcon }).bindPopup('<h1>Attack</h1><p>Hostiles</p>');
-  let mapCommsHostiles7 = L.marker(convertCoords(3558, 1005), { icon: commsIcon }).bindPopup('<h1>SFX</h1><p>Hostiles</p>');
-  let mapCommsHostiles8 = L.marker(convertCoords(3793, 1049), { icon: commsIcon }).bindPopup('<h1>Director</h1><p>Hostiles</p>');
-  let mapCommsHostiles9 = L.marker(convertCoords(3432, 708), { icon: commsIcon }).bindPopup('<h1>Guard Rails</h1><p>Hostiles</p>');
-  let mapCommsFriendly1 = L.marker(convertCoords(383, 1355), { icon: commsIcon }).bindPopup('<h1>Power Stations</h1><p>Friendly</p>');
-  let mapCommsFriendly2 = L.marker(convertCoords(740, 856), { icon: commsIcon }).bindPopup('<h1>Expectations</h1><p>Friendly</p>');
-  let mapCommsFriendly3 = L.marker(convertCoords(2324, 509), { icon: commsIcon }).bindPopup('<h1>Cleaners</h1><p>Friendly</p>');
-  let mapCommsFriendly4 = L.marker(convertCoords(3520, 1548), { icon: commsIcon }).bindPopup('<h1>Sacrifice</h1><p>Friendly</p>');
-  let mapCommsHistory1 = L.marker(convertCoords(548, 1663), { icon: commsIcon }).bindPopup('<h1>Territory</h1><p>History</p>');
-  let mapCommsHistory2 = L.marker(convertCoords(1466, 1921), { icon: commsIcon }).bindPopup('<h1>Hostage</h1><p>History</p>');
-  let mapCommsHistory3 = L.marker(convertCoords(1610, 1445), { icon: commsIcon }).bindPopup('<h1>Ready</h1><p>History</p>');
-  let mapCommsHistory4 = L.marker(convertCoords(1172, 1293), { icon: commsIcon }).bindPopup('<h1>Building Bridges</h1><p>History</p>');
-  let mapCommsHistory5 = L.marker(convertCoords(1248, 969), { icon: commsIcon }).bindPopup('<h1>Weapons</h1><p>History</p>');
-  let mapCommsHistory6 = L.marker(convertCoords(3257, 1247), { icon: commsIcon }).bindPopup('<h1>Police</h1><p>History</p>');
-  let mapCommsHistory7 = L.marker(convertCoords(2765, 692), { icon: commsIcon }).bindPopup('<h1>Free</h1><p>History</p>');
-  let mapCommsHistory8 = L.marker(convertCoords(3165, 550), { icon: commsIcon }).bindPopup('<h1>Baby</h1><p>History</p>');
+  let mapCommsBridge1 = L.marker(convertCoords(2233, 1155), { icon: commsIcon }).bindPopup(md.render('# Resources\n\nBridge'));
+  let mapCommsBridge2 = L.marker(convertCoords(2271, 1184), { icon: commsIcon }).bindPopup(md.render('# Fishy\n\nBridge'));
+  let mapCommsBridge3 = L.marker(convertCoords(2240, 1217), { icon: commsIcon }).bindPopup(md.render('# Sarah\n\nBridge'));
+  let mapCommsBridge4 = L.marker(convertCoords(2359, 1210), { icon: commsIcon }).bindPopup(md.render('# Reunited\n\nBridge'));
+  let mapCommsBridge5 = L.marker(convertCoords(2335, 1286), { icon: commsIcon }).bindPopup(md.render('# Maral\n\nBridge'));
+  let mapCommsBridge6 = L.marker(convertCoords(2229, 1338), { icon: commsIcon }).bindPopup(md.render('# Marauders\n\nBridge'));
+  let mapCommsHostiles1 = L.marker(convertCoords(1630, 2257), { icon: commsIcon }).bindPopup(md.render('# Negotiations\n\nHostiles'));
+  let mapCommsHostiles2 = L.marker(convertCoords(456, 1283), { icon: commsIcon }).bindPopup(md.render('# Tech\n\nHostiles'));
+  let mapCommsHostiles3 = L.marker(convertCoords(2721, 1379), { icon: commsIcon }).bindPopup(md.render('# Meret\n\nHostiles'));
+  let mapCommsHostiles4 = L.marker(convertCoords(2869, 1185), { icon: commsIcon }).bindPopup(md.render('# Collab\n\nHostiles'));
+  let mapCommsHostiles5 = L.marker(convertCoords(3556, 1326), { icon: commsIcon }).bindPopup(md.render('# Clinic\n\nHostiles'));
+  let mapCommsHostiles6 = L.marker(convertCoords(3700, 1257), { icon: commsIcon }).bindPopup(md.render('# Attack\n\nHostiles'));
+  let mapCommsHostiles7 = L.marker(convertCoords(3558, 1005), { icon: commsIcon }).bindPopup(md.render('# SFX\n\nHostiles'));
+  let mapCommsHostiles8 = L.marker(convertCoords(3793, 1049), { icon: commsIcon }).bindPopup(md.render('# Director\n\nHostiles'));
+  let mapCommsHostiles9 = L.marker(convertCoords(3432, 708), { icon: commsIcon }).bindPopup(md.render('# Guard Rails\n\nHostiles'));
+  let mapCommsFriendly1 = L.marker(convertCoords(383, 1355), { icon: commsIcon }).bindPopup(md.render('# Power Stations\n\nFriendly'));
+  let mapCommsFriendly2 = L.marker(convertCoords(740, 856), { icon: commsIcon }).bindPopup(md.render('# Expectations\n\nFriendly'));
+  let mapCommsFriendly3 = L.marker(convertCoords(2324, 509), { icon: commsIcon }).bindPopup(md.render('# Cleaners\n\nFriendly'));
+  let mapCommsFriendly4 = L.marker(convertCoords(3520, 1548), { icon: commsIcon }).bindPopup(md.render('# Sacrifice\n\nFriendly'));
+  let mapCommsHistory1 = L.marker(convertCoords(548, 1663), { icon: commsIcon }).bindPopup(md.render('# Territory\n\nHistory'));
+  let mapCommsHistory2 = L.marker(convertCoords(1466, 1921), { icon: commsIcon }).bindPopup(md.render('# Hostage\n\nHistory'));
+  let mapCommsHistory3 = L.marker(convertCoords(1610, 1445), { icon: commsIcon }).bindPopup(md.render('# Ready\n\nHistory'));
+  let mapCommsHistory4 = L.marker(convertCoords(1172, 1293), { icon: commsIcon }).bindPopup(md.render('# Building Bridges\n\nHistory'));
+  let mapCommsHistory5 = L.marker(convertCoords(1248, 969), { icon: commsIcon }).bindPopup(md.render('# Weapons\n\nHistory'));
+  let mapCommsHistory6 = L.marker(convertCoords(3257, 1247), { icon: commsIcon }).bindPopup(md.render('# Police\n\nHistory'));
+  let mapCommsHistory7 = L.marker(convertCoords(2765, 692), { icon: commsIcon }).bindPopup(md.render('# Free\n\nHistory'));
+  let mapCommsHistory8 = L.marker(convertCoords(3165, 550), { icon: commsIcon }).bindPopup(md.render('# Baby\n\nHistory'));
 
-  let mapControlPoint1 = L.marker(convertCoords(1676, 1887), { icon: controlPointIcon }).bindPopup('<h1>Cadman Plaza</h1><p>Rikers</p>');
-  let mapControlPoint2 = L.marker(convertCoords(1519, 697), { icon: controlPointIcon }).bindPopup('<h1>Fulton Ferry Market</h1><p>Rikers</p>');
-  let mapControlPoint3 = L.marker(convertCoords(2692, 541), { icon: controlPointIcon }).bindPopup('<h1>The Warehouse</h1><p>Cleaners</p>');
-  let mapControlPoint4 = L.marker(convertCoords(3241, 1101), { icon: controlPointIcon }).bindPopup('<h1>The Farm</h1><p>Cleaners</p>');
+  let mapControlPoint1 = L.marker(convertCoords(1676, 1887), { icon: controlPointIcon }).bindPopup(md.render('# Cadman Plaza\n\nRikers'));
+  let mapControlPoint2 = L.marker(convertCoords(1519, 697), { icon: controlPointIcon }).bindPopup(md.render('# Fulton Ferry Market\n\nRikers'));
+  let mapControlPoint3 = L.marker(convertCoords(2692, 541), { icon: controlPointIcon }).bindPopup(md.render('# The Warehouse\n\nCleaners'));
+  let mapControlPoint4 = L.marker(convertCoords(3241, 1101), { icon: controlPointIcon }).bindPopup(md.render('# The Farm\n\nCleaners'));
 
-  // let mapEcho1 = L.marker(convertCoords(0, 0), { icon: echoIcon }).bindPopup('<h1></h1>');
-  // let mapEcho2 = L.marker(convertCoords(0, 0), { icon: echoIcon }).bindPopup('<h1></h1>');
-  let mapEcho3 = L.marker(convertCoords(1710, 277), { icon: echoIcon })
-    .bindPopup('<h1>Faye</h1><p><strong>Requirements:</strong> Collect all 16 Danver\'s Network cards.</p><p><a href="https://redd.it/1kz9y7r">Full guide.</a></p>');
+  // let mapEcho1 = L.marker(convertCoords(0, 0), { icon: echoIcon }).bindPopup(md.render(''));
+  // let mapEcho2 = L.marker(convertCoords(0, 0), { icon: echoIcon }).bindPopup(md.render(''));
+  let mapEcho3 = L.marker(convertCoords(1710, 277), { icon: echoIcon }).bindPopup(md.render('# Faye\n\n**Requirements:** Collect all 16 Danver\'s Network cards.\n\n[Full guide.](https://redd.it/1kz9y7r)'));
 
   let mapHunterCipher = L.marker(convertCoords(2017, 636), { icon: hunterIcon })
-    .bindPopup('<h1>Hunter Spawn</h1><p><strong>Steps:</strong> Shoot the word "ROBOTO" on the AC units and the Hunter will spawn.</p><p><a href="https://redd.it/1kx76zh">Full guide.</a></p><p><strong>Mask:</strong> Cipher</p>');
+    .bindPopup(md.render('# Hunter Spawn\n\n**Steps:** Shoot the word "ROBOTO" on the AC units and the Hunter will spawn.\n\n[Full guide.](https://redd.it/1kx76zh)\n\n**Mask:** Cipher'));
   let mapHunterLumen = L.marker(convertCoords(1792, 745), { icon: hunterIcon })
-    .bindPopup('<h1>Hunter Spawn</h1><p><strong>Steps:</strong> Trip the switch inside the garage and follow the UV lights. The Hunter will spawn at the final location.</p><p><a href="https://redd.it/1kwzqez">Full guide.</a></p><p><strong>Mask:</strong> Lumen</p>');
+    .bindPopup(md.render('# Hunter Spawn\n\n**Steps:** Trip the switch inside the garage and follow the UV lights. The Hunter will spawn at the final location.\n\n[Full guide.](https://redd.it/1kwzqez)\n\n**Mask:** Lumen'));
   let mapHunterMerry = L.marker(convertCoords(1917, 323), { icon: hunterIcon })
-    .bindPopup('<h1>Hunter Spawn</h1><p><strong>Steps:</strong> Pick up the Legs, Torso, Tail and Head of a Merry-Go-Round horse and assemble them at the Merry-Go-Round where then the Hunter will spawn.</p><p><a href="https://redd.it/1kx6yjh">Full guide.</a></p><p><strong>Mask:</strong> Merry</p>');
+    .bindPopup(md.render('# Hunter Spawn\n\n**Steps:** Pick up the Legs, Torso, Tail and Head of a Merry-Go-Round horse and assemble them at the Merry-Go-Round where then the Hunter will spawn.\n\n[Full guide.](https://redd.it/1kx6yjh)\n\n**Mask:** Merry'));
   let mapHunterNecronos = L.marker(convertCoords(2886, 588), { icon: hunterIcon })
-    .bindPopup('<h1>Hunter Spawn</h1><p><strong>Requirements:</strong> Complete all prior 7 Hunter encounters.</p><p><strong>Steps:</strong> <a href="https://redd.it/1kyek7o">Full guide.</a></p><p><strong>Mask:</strong> Necronos</p>');
+    .bindPopup(md.render('# Hunter Spawn\n\n**Requirements:** Complete all prior 7 Hunter encounters.\n\n**Steps:** [Full guide.](https://redd.it/1kyek7o)\n\n**Mask:** Necronos'));
   let mapHunterRedtail = L.marker(convertCoords(2898, 823), { icon: hunterIcon })
-    .bindPopup('<h1>Hunter Spawn</h1><p><strong>Steps:</strong> <a href="https://redd.it/1kx49yp">Full guide.</a></p><p><strong>Mask:</strong> Redtail</p>');
+    .bindPopup(md.render('# Hunter Spawn\n\n**Steps:** [Full guide.](https://redd.it/1kx49yp)\n\n**Mask:** Redtail'));
   let mapHunterTracker = L.marker(convertCoords(3507, 1145), { icon: hunterIcon })
-    .bindPopup('<h1>Hunter Spawn</h1><p><strong>Steps:</strong> <a href="https://redd.it/1kye8fv">Full guide.</a></p><p><strong>Mask:</strong> Tracker</p>');
+    .bindPopup(md.render('# Hunter Spawn\n\n**Steps:** [Full guide.](https://redd.it/1kye8fv)\n\n**Mask:** Tracker'));
 
-  let mapMission1 = L.marker(convertCoords(656, 1364), { icon: missionIcon })
-    .bindPopup('<h1>Bridge Park Pier</h1><p><strong>Type:</strong> World Mission</p><p><strong>Bosses:</strong> Bombshell</p>');
-  let mapMission2 = L.marker(convertCoords(1044, 2025), { icon: missionIcon })
-    .bindPopup('<h1>Clark Street</h1><p><strong>Type:</strong> World Mission</p><p><strong>Bosses:</strong> Auto</p>');
-  let mapMission3 = L.marker(convertCoords(3715, 902), { icon: missionIcon })
-    .bindPopup('<h1>DUMBO Skate Park</h1><p><strong>Type:</strong> World Mission</p><p><strong>Bosses:</strong> Hollings</p>');
-  let mapMission4 = L.marker(convertCoords(3581, 1535), { icon: missionIcon })
-    .bindPopup('<h1>CERA Clinic</h1><p><strong>Type:</strong> World Mission</p><p><strong>Bosses:</strong> Stewart</p>');
+  let mapMission1 = L.marker(convertCoords(656, 1364), { icon: missionIcon }).bindPopup(md.render('# Bridge Park Pier\n\n**Type:** World Mission\n\n**Boss:** Bombshell'));
+  let mapMission2 = L.marker(convertCoords(1044, 2025), { icon: missionIcon }).bindPopup(md.render('# Clark Street\n\n**Type:** World Mission\n\n**Boss:** Auto'));
+  let mapMission3 = L.marker(convertCoords(3715, 902), { icon: missionIcon }).bindPopup(md.render('# DUMBO Skate Park\n\n**Type:** World Mission\n\n**Boss:** Hollings'));
+  let mapMission4 = L.marker(convertCoords(3581, 1535), { icon: missionIcon }).bindPopup(md.render('# CERA Clinic\n\n**Type:** World Mission\n\n**Boss:** Stewart'));
 
   let mapNamedElite1 = L.marker(convertCoords(771, 1259), { icon: namedEliteIcon })
-    .bindPopup('<h1>Named Elite</h1><p>Danver Network card farming spot. Every 30 minutes a random boss will spawn here and drop one of the 16 Danver\'s Network cards.</p>');
+    .bindPopup(md.render('# Named Elite\n\nDanver\'s Network card farming spot. Every 30 minutes a random boss will spawn here and drop one of the 16 Danver\'s Network cards.'));
   let mapNamedElite2 = L.marker(convertCoords(1086, 548), { icon: namedEliteIcon })
-    .bindPopup('<h1>Named Elite</h1><p>Danver Network card farming spot. Every 30 minutes a random boss will spawn here and drop one of the 16 Danver\'s Network cards.</p>');
+    .bindPopup(md.render('# Named Elite\n\nDanver\'s Network card farming spot. Every 30 minutes a random boss will spawn here and drop one of the 16 Danver\'s Network cards.'));
   let mapNamedElite3 = L.marker(convertCoords(1145, 1071), { icon: namedEliteIcon })
-    .bindPopup('<h1>Named Elite</h1><p>Danver Network card farming spot. Every 30 minutes a random boss will spawn here and drop one of the 16 Danver\'s Network cards.</p>');
+    .bindPopup(md.render('# Named Elite\n\nDanver\'s Network card farming spot. Every 30 minutes a random boss will spawn here and drop one of the 16 Danver\'s Network cards.'));
   let mapNamedElite4 = L.marker(convertCoords(2013, 310), { icon: namedEliteIcon })
-    .bindPopup('<h1>Named Elite</h1><p>Danver Network card farming spot. Every 30 minutes a random boss will spawn here and drop one of the 16 Danver\'s Network cards.</p>');
+    .bindPopup(md.render('# Named Elite\n\nDanver\'s Network card farming spot. Every 30 minutes a random boss will spawn here and drop one of the 16 Danver\'s Network cards.'));
   let mapNamedElite5 = L.marker(convertCoords(3403, 590), { icon: namedEliteIcon })
-    .bindPopup('<h1>Named Elite</h1><p>Danver Network card farming spot. Every 30 minutes a random boss will spawn here and drop one of the 16 Danver\'s Network cards.</p>');
+    .bindPopup(md.render('# Named Elite\n\nDanver\'s Network card farming spot. Every 30 minutes a random boss will spawn here and drop one of the 16 Danver\'s Network cards.'));
   let mapNamedElite6 = L.marker(convertCoords(2710, 1417), { icon: namedEliteIcon })
-    .bindPopup('<h1>Named Elite</h1><p><strong>Location:</strong> Underground</p><p>Danver Network card farming spot. Every 30 minutes a random boss will spawn here and drop one of the 16 Danver\'s Network cards.</p>');
+    .bindPopup(md.render('# Named Elite\n\n**Location:** Underground\n\nDanver Network card farming spot. Every 30 minutes a random boss will spawn here and drop one of the 16 Danver\'s Network cards.'));
   let mapNamedElite7 = L.marker(convertCoords(3241, 1489), { icon: namedEliteIcon })
-    .bindPopup('<h1>Named Elite</h1><p>Danver Network card farming spot. Every 30 minutes a random boss will spawn here and drop one of the 16 Danver\'s Network cards.</p>');
+    .bindPopup(md.render('# Named Elite\n\nDanver\'s Network card farming spot. Every 30 minutes a random boss will spawn here and drop one of the 16 Danver\'s Network cards.'));
 
-  let mapSafeHouse1 = L.marker(convertCoords(1162, 1291), { icon: safeHouseIcon }).bindPopup('<h1>The Lookout</h1><p>Safe House</p>');
-  let mapSafeHouse2 = L.marker(convertCoords(3178, 517), { icon: safeHouseIcon }).bindPopup('<h1>The Retreat</h1><p>Safe House</p>');
+  let mapSafeHouse1 = L.marker(convertCoords(1162, 1291), { icon: safeHouseIcon }).bindPopup(md.render('# The Lookout\n\nSafe House'));
+  let mapSafeHouse2 = L.marker(convertCoords(3178, 517), { icon: safeHouseIcon }).bindPopup(md.render('# The Retreat\n\nSafe House'));
 
-  let mapSettlement1 = L.marker(convertCoords(2305, 1244), { icon: settlementBridgeIcon }).bindPopup('<h1>The Bridge</h1><p>Settlement</p>');
+  let mapSettlement1 = L.marker(convertCoords(2305, 1244), { icon: settlementBridgeIcon }).bindPopup(md.render('# The Bridge\n\nSettlement'));
 
   // Markers: Overlays
 
